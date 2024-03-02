1b48cf551de3be705203a7266a504a9a52c8aa0e
Satyajit Lele (Jeet) <98499268+JeetRH@users.noreply.github.com>
2024-03-02 14:40:46 -0500

    Create plots.py

---
+++ plots.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import altair as alt
+
+# This generates random data with 100 rows and 3 columns! Pretty cool
+data_np = pd.DataFrame(
+    np.random.randn(100,3),
+    columns=['hot','cold','warm']
+)
+
+#st.write(data_np)
+
+#st.line_chart(data_np)
+
+#st.area_chart(data_np)
+
+#st.bar_chart(data_np)
+
+plt.scatter(data_np['cold'],data_np['warm'])
+plt.title("scatter")
+st.pyplot()
+
+chart = alt.Chart(data_np).mark_circle().encode(
+    x = 'cold', y='hot'
+)
+st.altair_chart(chart)
