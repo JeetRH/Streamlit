3fbdcbce4680314d90103790de4c0412bd00c7c3
Jeet <slele@redhat.com>
2024-03-06 17:17:32 -0500

    add black as deev package and add init

--- demo.py
+++ demo.py
@@ -3,8 +3,12 @@ from datetime import date
 import pandas as pd
 import numpy as np
 
+
 def creds_entered():
-    if st.session_state["user"].strip() == "admin" and st.session_state["passwd"].strip() == "admin":
+    if (
+        st.session_state["user"].strip() == "admin"
+        and st.session_state["passwd"].strip() == "admin"
+    ):
         st.session_state["authenticated"] = True
     else:
         st.session_state["authenticated"] = False
@@ -13,41 +17,74 @@ def creds_entered():
         elif not st.session_state["user"]:
             st.warning["Please enter username"]
         else:
-            st.error("Invalid Username/Pass :face_with_raised_eyebrow: :man-shrugging: :yawning_face:")
+            st.error(
+                "Invalid Username/Pass :face_with_raised_eyebrow: :man-shrugging: :yawning_face:"
+            )
+
 
 def authenticate_user():
     if "authenticated" not in st.session_state:
         st.text_input(label="Username:", value="", key="user", on_change=creds_entered)
-        st.text_input(label="Password:", value="", key="passwd", type="password", on_change=creds_entered)
+        st.text_input(
+            label="Password:",
+            value="",
+            key="passwd",
+            type="password",
+            on_change=creds_entered,
+        )
     else:
         if st.session_state["authenticated"]:
             return True
         else:
-            st.text_input(label="Username:", value="", key="user", on_change=creds_entered)
-            st.text_input(label="Password:", value="", key="passwd", type="password", on_change=creds_entered)
+            st.text_input(
+                label="Username:", value="", key="user", on_change=creds_entered
+            )
+            st.text_input(
+                label="Password:",
+                value="",
+                key="passwd",
+                type="password",
+                on_change=creds_entered,
+            )
             return False
 
-if authenticate_user():
 
-    st.title("Hello :red[streamlit] :100: :the_horns:")
+def streamlit_app():
+    if authenticate_user():
+        st.title("Hello :red[streamlit] :100: :the_horns:")
 
-    st.header("Header :anchor:")
+        st.header("Header :anchor:")
 
-    st.subheader("Sub Header :taurus:")
+        st.subheader("Sub Header :taurus:")
 
-    st.text('This is a trial of text')
+        st.text("This is a trial of text")
 
-    st.latex(r'''
-        a + ar + a r^2 + a r^3 ''',help='This is latex function to display mathematical functions')
+        st.latex(
+            r"""
+            a + ar + a r^2 + a r^3 """,
+            help="This is latex function to display mathematical functions",
+        )
 
-    st.markdown(""" ### h3 tag :moon: :sunglasses: :cool: """)
+        st.markdown(""" ### h3 tag :moon: :sunglasses: :cool: """)
 
-    df = pd.DataFrame.from_dict({ 'name': ['Yoda', 'John Wick', 'Pikachu'],
-           'country': ['Star', 'USA', 'Japan'],
-           'dob': [date.today().strftime("%B %d, %Y"), date(2002,5,5), date(1992,12,12)] })
+        df = pd.DataFrame.from_dict(
+            {
+                "name": ["Yoda", "John Wick", "Pikachu"],
+                "country": ["Star", "USA", "Japan"],
+                "dob": [
+                    date.today().strftime("%B %d, %Y"),
+                    date(2002, 5, 5),
+                    date(1992, 12, 12),
+                ],
+            }
+        )
 
-    st.write(df)
+        st.write(df)
 
-    data_csv = pd.read_csv("data//Salary_Data.csv")
+        data_csv = pd.read_csv("data//Salary_Data.csv")
 
-    st.dataframe(data_csv,width=1500,height=300)
+        st.dataframe(data_csv, width=1500, height=300)
+
+
+if __name__ == "__main__":
+    streamlit_app()
--- pages/2_data.py
+++ pages/2_data.py
@@ -5,7 +5,10 @@ import numpy as np
 
 
 def creds_entered():
-    if st.session_state["user"].strip() == "admin" and st.session_state["passwd"].strip() == "admin":
+    if (
+        st.session_state["user"].strip() == "admin"
+        and st.session_state["passwd"].strip() == "admin"
+    ):
         st.session_state["authenticated"] = True
     else:
         st.session_state["authenticated"] = False
@@ -14,30 +17,48 @@ def creds_entered():
         elif not st.session_state["user"]:
             st.warning["Please enter username"]
         else:
-            st.error("Invalid Username/Pass :face_with_raised_eyebrow: :man-shrugging: :yawning_face:")
+            st.error(
+                "Invalid Username/Pass :face_with_raised_eyebrow: :man-shrugging: :yawning_face:"
+            )
+
 
 def authenticate_user():
     if "authenticated" not in st.session_state:
         st.text_input(label="Username:", value="", key="user", on_change=creds_entered)
-        st.text_input(label="Password:", value="", key="passwd", type="password", on_change=creds_entered)
+        st.text_input(
+            label="Password:",
+            value="",
+            key="passwd",
+            type="password",
+            on_change=creds_entered,
+        )
     else:
         if st.session_state["authenticated"]:
             return True
         else:
-            st.text_input(label="Username:", value="", key="user", on_change=creds_entered)
-            st.text_input(label="Password:", value="", key="passwd", type="password", on_change=creds_entered)
+            st.text_input(
+                label="Username:", value="", key="user", on_change=creds_entered
+            )
+            st.text_input(
+                label="Password:",
+                value="",
+                key="passwd",
+                type="password",
+                on_change=creds_entered,
+            )
             return False
 
+
 if authenticate_user():
-    a = [4,5,6,98,45,36,22,78]
+    a = [4, 5, 6, 98, 45, 36, 22, 78]
 
     n = np.array(a)
 
-    nd = n.reshape((2,4))
+    nd = n.reshape((2, 4))
 
     data_csv = pd.read_csv("..//data//Salary_Data.csv")
 
-    st.dataframe(data_csv,width=1500,height=300)
+    st.dataframe(data_csv, width=1500, height=300)
     # same output can be achieved through st.write
 
 # We also have st.table to print out the whole table and st.json to print json output like dictionaries
